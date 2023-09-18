6fc808a9856e42e40e00551e3068c8f1f9172a96
melshinawy <melshinawy@users.noreply.github.com>
2023-09-18 20:37:38 +0300

    start with models

--- jobsearch/models.py
+++ jobsearch/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-# class User(AbstractUser):
-#     class Role(models.TextChoices):
-#         ADMIN = "ADMIN", 'Admin'
-#         EMPLOYER = "EMPLOYER", 'Employer'
-#         JOBSEEKER = "JOBSEEKER", 'Jobseeker'
-    
-#     base_role = Role.ADMIN
+class User(AbstractUser):
+    pass
 
-#     role = models.CharField(max_length=50, choices=Role.choices)
+class Company(models.Model):
+    account = models.ForeignKey(User, unique=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+
+class Jobseeker(models.Model):
+    account = models.ForeignKey(User, unique=True, on_delete=models.CASCADE)
+
+class Job(models.model):
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    applicant = models.ManyToManyField(Jobseeker, blank=True, null=True, on_delete=models.SET_NULL)
